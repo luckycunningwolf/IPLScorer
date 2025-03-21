0ef240f94975ea4a62f55d4c33af0d9aa19aa9ce
luckycunningwolf <134157294+luckycunningwolf@users.noreply.github.com>
2025-03-21 20:05:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext, CallbackQueryHandler
 
 
 import os
